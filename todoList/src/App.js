c0f39fff14606e06ae5fb5f66ee8344f7f8f630f
hyeonjin <hyeonjin9819@gmail.com>
2022-07-19 16:28:26 +0900

    간단 todolist
    간단 todolist react 사용해 구현

---
+++ todoList/src/App.js
@@ -0,0 +1,19 @@
+import {useState, useEffect} from "react";
+
+function App() {
+  const[loading, setLoading] = useState(true);
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+    .then(response => response.json())
+    .then((json) => console.log(json));
+  },[]);
+  return (
+   <div>
+    <h1>The Coins!</h1>
+    {loading ? <strong>Loading...</strong>: null}
+    </div>
+  );
+}
+
+export default App;
+
